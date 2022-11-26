af8472b1a82dd140789afd8cd0649c5dfeec5fe3
Arkapravo-Ghosh <ghosharkapravo1612005@gmail.com>
2022-11-26 22:15:50 +0530

    Update

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+echo "Installing..."
 pyinstaller --onefile --windowed src/main.py --name fan-mode
 sudo cp dist/fan-mode /usr/bin/fan-mode
+echo "Done."
--- uninstall.sh
+++ uninstall.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-sudo rm -f /usr/bin/fan-mode
+while true; do
+    read -p "Do you wish to uninstall this program? [y/N]: " yn
+    case $yn in
+        [Yy]* ) sudo rm -f /usr/bin/fan-mode && echo "Uninstalled."; break;;
+        [Nn]* ) echo "Aborted."; exit;;
+        * ) echo "Aborted."; exit;;
+    esac
+done
