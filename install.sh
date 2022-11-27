967b7985e819006280320812b6cd26cb3a9eeeed
Arkapravo-Ghosh <ghosharkapravo1612005@gmail.com>
2022-11-27 13:47:17 +0530

    Updated Installer

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-echo "Installing..."
-pyinstaller --onefile --windowed src/main.py --name fan-mode
-sudo cp dist/fan-mode /usr/bin/fan-mode
-echo "Done."
+while true; do
+    read -p "Do you wish to install this program? [y/N]: " yn
+    case $yn in
+        [Yy]* ) echo "Installing..." && pyinstaller --onefile --windowed src/main.py --name fan-mode && sudo cp dist/fan-mode /usr/bin/fan-mode && echo "Installed."; break;;
+        [Nn]* ) echo "Aborted."; exit;;
+        * ) echo "Aborted."; exit;;
+    esac
+done
