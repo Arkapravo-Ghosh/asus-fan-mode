108ca42f4f2e3d2f5707adfef02dbf42eafd35ca
Gerald-H <53166232+Gerald-Ha@users.noreply.github.com>
2024-10-06 08:20:42 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,32 @@
 while true; do
     read -p "Do you wish to install this program? [y/N]: " yn
     case $yn in
-        [Yy]* ) echo "Installing..."; rm -rf build dist fan-mode.spec; pyinstaller --noconfirm --onedir --windowed src/main.py --name fan-mode; sudo rm -rf /opt/fan-mode /usr/bin/fan-mode; sudo cp -r dist/fan-mode /opt/fan-mode; sudo ln -s /opt/fan-mode/fan-mode /usr/bin/fan-mode; sudo cp fan-mode-recovery.service /etc/systemd/system/; sudo systemctl daemon-reload; sudo systemctl enable fan-mode-recovery; echo "Installed."; break;;
-        [Nn]* ) echo "Aborted."; exit;;
-        * ) echo "Aborted."; exit;;
+        [Yy]* ) 
+            echo "Installing..."
+
+            
+            sudo rm -rf /opt/fan-mode /usr/bin/fan-mode
+
+            
+            sudo mkdir -p /opt/fan-mode
+            sudo cp -r src/main.py /opt/fan-mode/fan-mode.py
+
+            
+            sudo ln -s /opt/fan-mode/fan-mode.py /usr/bin/fan-mode
+
+            
+            sudo cp fan-mode-recovery.service /etc/systemd/system/
+            sudo systemctl daemon-reload
+            sudo systemctl enable fan-mode-recovery
+            sudo systemctl start fan-mode-recovery
+
+            echo "Installed."
+            break;;
+        [Nn]* ) 
+            echo "Aborted."
+            exit;;
+        * ) 
+            echo "Aborted."
+            exit;;
     esac
 done
